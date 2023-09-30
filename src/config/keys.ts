507859955c8bd64e6a5aee1ab0e4be385950af00
vernyuy <fonchu.e.venyuy@gmail.com>
2023-09-30 10:53:30 +0100

    nestJs api crash

---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoURI:
+    'mongodb+srv://fonchu:N0lS7fOsfWEZObH4@cluster0.w5gy7jz.mongodb.net/?retryWrites=true&w=majority',
+};
---
+++ src/items/items.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Delete,
+  Param,
+  Put,
+} from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+  @Get()
+  findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id): Promise<Item> {
+    return this.itemsService.findOne(id);
+  }
+  @Post()
+  async create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return await this.itemsService.create(createItemDto);
+  }
+
+  @Put(':id')
+  async update(
+    @Body() updateItemDto: CreateItemDto,
+    @Param('id') id,
+  ): Promise<Item> {
+    return await this.itemsService.update(id, updateItemDto);
+  }
+
+  @Delete(':id')
+  async deleteItem(@Param('id') id): Promise<Item> {
+    return await this.itemsService.delete(id);
+  }
+}
